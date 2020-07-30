0be3dfb29d47e118a71057fb402e88288be98094
Vikram Choudhary <vikramus4@gmail.com>
2020-07-30 16:08:42 +0530

    day 15 solution

--- 30Days-of-Code/30-days-of-code.py
+++ 30Days-of-Code/30-days-of-code.py
@@ -260,3 +260,35 @@ print(d.maximumDifference)
 
 
 # Day 15: Linked List
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+        newNode = Node(data)
+        if head is None: return newNode
+        temp = head
+        while temp.next:
+                temp = temp.next
+        temp.next = newNode
+        return head
+    #Complete this method
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head)
+
+
+# Day 16: Exceptions - String to Integer
