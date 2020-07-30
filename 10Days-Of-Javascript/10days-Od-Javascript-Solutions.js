49324967f729dbcbf64a81258cdcf999b8627595
Vikram Choudhary <vikramus4@gmail.com>
2020-07-30 16:49:55 +0530

    javascript solutions updated

--- 10Days-Of-Javascript/10days-Od-Javascript-Solutions.js
+++ 10Days-Of-Javascript/10days-Od-Javascript-Solutions.js
@@ -162,5 +162,398 @@ function main() {
 }
 
 
-//
+// Day 2: Loops
 
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    let nonVowels = "";
+    [...s].forEach(a => "aeiou".includes(a) ? console.log(a) : nonVowels += a + '\n')
+    console.log(nonVowels)
+
+}
+
+function main() {
+    const s = readLine();
+
+    vowelsAndConsonants(s);
+}
+
+
+// Day 3: Arrays
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    return Array.from(new Set(nums)).sort((a, b) => { return b - a })[1]
+}
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+
+    console.log(getSecondLargest(nums));
+}
+
+
+// Day 3: Try, Catch, and Finally
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try {
+        s = s.split('').reverse().join('');
+    }
+    catch (e) {
+        console.log(e.message)
+    }
+    finally {
+        console.log(s)
+    }
+}
+
+
+// Day 3: Throw
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    if (a <= 0) {
+        throw (a === 0 ? new Error("Zero Error") :
+            new Error("Negative Error"))
+    }
+    else {
+        return "YES"
+    }
+}
+
+
+// Day 4: Create a Rectangle Object
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    return { "length": a, "width": b, "perimeter": 2 * a + 2 * b, "area": a * b }
+}
+
+
+// Day 4: Count Objects
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    return objects.filter(ele => { return ele.x === ele.y }).length
+}
+
+
+//Day 4: Classes
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+class Polygon {
+    constructor(lens) {
+        this.lengths = lens
+    }
+    perimeter() {
+        return this.lengths.reduce((a, b) => { return a + b })
+    }
+}
+
+
+// Day 5: Inheritance
+
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+Rectangle.prototype.area = function () {
+    return (this.w * this.h)
+}
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+class Square extends Rectangle {
+    constructor(s) {
+        super(s, s);
+    }
+}
+
+if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify(['constructor'])) {
+    const rec = new Rectangle(3, 4);
+    const sqr = new Square(3);
+
+    console.log(rec.area());
+    console.log(sqr.area());
+} else {
+    console.log(-1);
+    console.log(-1);
+}
+
+
+// Day 5: Template Literals
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    const [a, p] = expressions;
+    const root = Math.sqrt((p * p) - (16 * a));
+    const s1 = (p + root) / 4;
+    const s2 = (p - root) / 4
+    return [s2, s1]
+}
+
+
+// Day 5: Arrow Functions
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    return nums.map(ele => {
+        if (ele % 2 == 0) ele * 2
+        else ele * 3
+    })
+}
+
+
+// Day 6: Bitwise Operators
+
+function getMaxLessThanK(n, k) {
+    return ((k | k - 1) > n) ? (k - 2) : (k - 1)
+}
+
+
+// Day 6: JavaScript Dates
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName = {
+        0: "Sunday",
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday"
+    };
+    let day = new Date(dateString).getUTCDay();
+    return dayName[day];
+}
+
+
+// Day 7: Regular Expressions I
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+
+    const re = new RegExp('^([aeiou]).+\\1$')
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+// Day 7: Regular Expressions II
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
+     */
+    let re = new RegExp('^(Mr|Mrs|Ms|Dr|Er)(\\.)([a-zA-z])*$')
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+// Day 7: Regular Expressions III
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+
+    let re = new RegExp('\\d+', 'g')
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+// Day 8: Create a Button  --> 3 Files
+
+// index.html   --> file
+
+// <!DOCTYPE html>
+{/* <html>
+    <head>
+        <link rel="stylesheet" href="css/button.css" type="text/css">
+    </head>
+    
+    <body>
+    	<script src="js/button.js" type="text/javascript"></script>
+    </body>
+</html> */}
+
+// js/button.js  --> file
+
+let btn = document.createElement("Button");
+
+btn.innerHTML = "0";
+btn.id = "btn";
+btn.className = "btnClass";
+
+document.body.appendChild(btn);
+
+btn.onclick = function () {
+    btn.innerHTML++;
+}
+
+// css/button.css --> file
+
+// .btnClass {
+//     width: 96px;
+//     height: 48px;
+//     font-size: 24px;
+// }
+
+
+// Day 8: Buttons Container --> 3 Files
+
+//index.html --> file
+
+{/* <body>
+    <script src="js/buttonsGrid.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="css/buttonsGrid.css" type="text/css">
+    
+    <div id="btns" class="btnContainer">
+		<button id="btn1" class="btnStyle">1</button>
+		<button id="btn2" class="btnStyle">2</button>
+		<button id="btn3" class="btnStyle">3</button>
+		<button id="btn4" class="btnStyle">4</button>
+        <button id="btn5" class="btnStyle" onClick="rotate()">5</button>
+        <button id="btn6" class="btnStyle">6</button>
+        <button id="btn7" class="btnStyle">7</button>
+        <button id="btn8" class="btnStyle">8</button>
+        <button id="btn9" class="btnStyle">9</button>
+	</div>
+</body> */}
+
+//js/buttonsGrid.js --> file
+
+let l = "4";
+let a = ["1", "2", "3", "6", "9", "8", "7", "4"];
+let b = ["1", "2", "3", "6", "9", "8", "7", "4"];
+
+let rotate = function () {
+    for (let i = 7; i > 0; i--) {
+        a[i] = a[i - 1];
+    }
+
+    a[0] = l;
+    l = a[7];
+
+    for (let i = 0; i < 8; i++) {
+        document.getElementById("btn" + b[i]).innerText = a[i];
+    }
+}
+
+//css/buttonsGrid.css --> file
+
+// .btnContainer {
+//     width: 75%;
+// }
+
+// .btnContainer > .btnStyle {
+//     width: 30%;
+//     height: 48px;
+//     font-size: 24px;
+// }
+
+//  Day 9: Binary Calculator --> 3 files
